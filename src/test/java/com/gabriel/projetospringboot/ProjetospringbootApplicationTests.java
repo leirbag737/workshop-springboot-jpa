7ebc15816dd7d35f077177da621fa4840ebeada4
Gabriel de Aguiar <gabrieldeaguiarbrito737@gmail.com>
2023-02-09 14:35:18 -0300

    Project created

---
+++ src/test/java/com/gabriel/projetospringboot/ProjetospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.projetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
