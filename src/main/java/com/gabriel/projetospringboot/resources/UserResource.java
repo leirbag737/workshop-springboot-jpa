467f422a9cd82d9fbe9a3e1e33a274044c52e283
Gabriel de Aguiar <gabrieldeaguiarbrito737@gmail.com>
2023-02-25 22:33:12 -0300

    Order, Instant, ISO 8601 - part 1

--- src/main/java/com/gabriel/projetospringboot/resources/UserResource.java
+++ src/main/java/com/gabriel/projetospringboot/resources/UserResource.java
@@ -14,7 +14,7 @@ import com.gabriel.projetospringboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResources {
+public class UserResource {
 
     @Autowired
     private UserService service;
